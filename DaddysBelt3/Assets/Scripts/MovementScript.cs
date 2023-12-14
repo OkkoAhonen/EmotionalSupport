f31a4703d9cb3a2d74ff28bb5cdb59c52ec10b03
Okko Ahonen <okko.a.ahonen@gmail.com>
2023-12-14 15:27:10 +0200

    movesc

--- DaddysBelt3/Assets/Scripts/MovementScript.cs
+++ DaddysBelt3/Assets/Scripts/MovementScript.cs
@@ -4,8 +4,9 @@ public class MovementScript : MonoBehaviour
 {
     public float normaaliNopeus = 10f; // Normaali liikkumisnopeus
     public float juoksuNopeus = 100f; // Nopeampi juoksu
-
     public float stamina = 100f;
+    public float staminaVahinko = 10f; // Kuinka paljon staminaa vähennetään per juoksukerta
+    public float staminaPalautus = 5f; // Kuinka paljon staminaa palautuu, kun ei juosta
 
     private Rigidbody2D rb;
     private float currentSpeed; // Nykyinen nopeus
@@ -26,20 +27,20 @@ public class MovementScript : MonoBehaviour
         rb.MovePosition(rb.position + liike);
 
         // Juoksu
-        if (Input.GetKeyDown(KeyCode.LeftShift)) 
+        if (Input.GetKey(KeyCode.LeftShift) && stamina > 0f)
         {
-            staminaDrain();
-
             currentSpeed = juoksuNopeus;
+            stamina -= staminaVahinko * Time.deltaTime; // Vähennä staminaa juostessa
+            stamina = Mathf.Clamp(stamina, 0f, 100f); // Varo, ettei stamina mene negatiiviseksi tai yli maksimin
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        else
         {
             currentSpeed = normaaliNopeus;
+            if (stamina < 100f)
+            {
+                stamina += staminaPalautus * Time.deltaTime; // Palauta staminaa hitaasti, kun ei juosta
+                stamina = Mathf.Clamp(stamina, 0f, 100f); // Varo, ettei stamina ylitä maksimia
+            }
         }
-    }
-
-    void staminaDrain()
-    {
-        stamina -= 1;
     }
 }

05b9613e7a68937ecfb1d56655568aee84a5425e
Trollipopper <113014080+Trollipopper@users.noreply.github.com>
2023-12-14 14:01:47 +0200

    Daddy's stuff

---
+++ DaddysBelt3/Assets/Scripts/DrugScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrugScript : MonoBehaviour
+{
+    public float angle;
+
+    public void Setup(float throwDir)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
